4e63c8ad8ccd84b5f60bf41c71673ccc41a74917
Bryce Palichuk <43261252+Brycepalichuk@users.noreply.github.com>
2022-04-15 16:00:17 -0600

    Add "Enter App" button and Favicon link
    Added the Enter app button to the hamburger menu and the favicon link

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import {
   Error
 } from "./components";
 import './components/Style.css'
-import { House, Group, ContactSupport, Map, Help, Extension, EmojiObjects } from '@material-ui/icons'; //Work=Careers and Assignment=Whitepaper
+import { House, Group, ContactSupport, Map, Help, Extension, EmojiObjects, ArrowForward } from '@material-ui/icons'; //Work=Careers and Assignment=Whitepaper
 import Logo from './components/images/3DPrintable_Logo_WhiteType.png'; // Logo
 
 const App = () => {
@@ -56,131 +56,131 @@ const App = () => {
 
                     {/* Start mobile */}
                     <div class="menu-extras">
-                    <div class="menu-item">
-                    <a href="#/" onClick={handleToggle}>{navbarOpen}
-                        <div class="toggle-container">
-                            <div class="bar1"></div>
-                            <div class="bar2"></div>
-                            <div class="bar3"></div>
+                        <div class="menu-item">
+                            <a href="#/" onClick={handleToggle}>{navbarOpen}
+                                <div class="toggle-container">
+                                    <div class="bar1"></div>
+                                    <div class="bar2"></div>
+                                    <div class="bar3"></div>
+                                </div>
+                            </a>
+
+                            <ul className={`menuNav ${navbarOpen ? " showMenu" : ""} hideBar`}>
+                                <li><House /><a href='/'>Home</a></li>
+                                <li><EmojiObjects/><a href="About" class="sub-menu-item"> About </a></li>
+                                <li><Extension/><a href="Features" class="sub-menu-item"> Features</a></li>
+                                <li><Group/><a href="Team" class="sub-menu-item"> Team</a></li>
+                                <li><Map/><a href="Roadmap" class="sub-menu-item"> Roadmap</a></li>
+                                {/* <li><Work/><a href="Careers" class="sub-menu-item"> Career</a></li> */}
+                                <li><Help/><a href="FAQs" class="sub-menu-item">FAQs </a></li>
+                                {/* <li><Assignment/><a href="Whitepaper" class="sub-menu-item">Whitepaper </a></li> */}
+                                <li><ContactSupport/><a href="Contact" class="sub-menu-item">Contact Us</a></li>
+                                <li><ArrowForward/><a href="https://app.3dprintable.io" target="_blank" rel="noopener noreferrer" id="a">Enter App</a></li>
+                            </ul>
                         </div>
-                    </a>
-                    <ul className={`menuNav ${navbarOpen ? " showMenu" : ""} hideBar`}>
-                        <li>
-                            <House /><a href='/'>Home</a>
-                        </li>
-                        <li><EmojiObjects/><a href="About" class="sub-menu-item"> About </a></li>
-                        <li><Extension/><a href="Features" class="sub-menu-item"> Features</a></li>
-                        <li><Group/><a href="Team" class="sub-menu-item"> Team</a></li>
-                        <li><Map/><a href="Roadmap" class="sub-menu-item"> Roadmap</a></li>
-                        {/* <li><Work/><a href="Careers" class="sub-menu-item"> Career</a></li> */}
-                        <li><Help/><a href="FAQs" class="sub-menu-item">FAQs </a></li>
-                        {/* <li><Assignment/><a href="Whitepaper" class="sub-menu-item">Whitepaper </a></li> */}
-                        <li><ContactSupport/><a href="Contact" class="sub-menu-item">Contact Us</a></li>
-                    </ul>
-                        
-                    </div>
                     </div>
                     {/* End mobile */}
                     
 
                     {/* Start navigation */}
                     <div id="navigation">
-                            <ul class="navigation-menu nav-right nav-light">
-                                    <li>
-                                        <a href="/" class="sub-menu-item">
-                                            <button class="btns btns-outline-header">Home
-                                            </button>
-                                            </a>
-                                    </li>
-                                    
-                                    <li class="has-submenu parent-parent-menu-item">
-                                            <a href="/#" id="a">
+                        <ul class="navigation-menu nav-right nav-light">
+                                <li>
+                                    <a href="/" class="sub-menu-item">
+                                        <button class="btns btns-outline-header">
+                                            Home
+                                        </button>
+                                    </a>
+                                </li>
+                                
+                                <li class="has-submenu parent-parent-menu-item">
+                                        <a href="/#" id="a">
                                             <button type="button" class="btns btns-outline-header dropdown-toggle" aria-haspopup="true">
                                                 Pages
                                             </button>
-                                            </a>
-                                            
-                                            <ul class="submenu" >
-                                                    <li><a href="About" class="sub-menu-item"> About</a></li>
-                                                    <li><a href="Features" class="sub-menu-item"> Features</a></li>
-                                                    <li><a href="Team" class="sub-menu-item"> Team</a></li>
-                                                    <li><a href="Roadmap" class="sub-menu-item"> Roadmap</a></li>
-                                                    {/* <li><a href="Careers" class="sub-menu-item"> Career</a></li> */}
-                                                    <li><a href="FAQs" class="sub-menu-item">FAQs </a></li>
-                                                    {/* <li><a href="Whitepaper" class="sub-menu-item">Whitepaper </a></li> */}
-                                                    <li><a href="Contact" class="sub-menu-item">Contact Us</a></li>
-                                            </ul>
-                                    </li>
-                                <li>
-                                    <a href="https://app.3dprintable.io" target="_blank" rel="noopener noreferrer" id="a" >
-                                        <button class="btn btn-primary">Enter App</button>
-                                    </a>
+                                        </a>
+                                        
+                                        <ul class="submenu" >
+                                            <li><a href="About" class="sub-menu-item"> About</a></li>
+                                            <li><a href="Features" class="sub-menu-item"> Features</a></li>
+                                            <li><a href="Team" class="sub-menu-item"> Team</a></li>
+                                            <li><a href="Roadmap" class="sub-menu-item"> Roadmap</a></li>
+                                            {/* <li><a href="Careers" class="sub-menu-item"> Career</a></li> */}
+                                            <li><a href="FAQs" class="sub-menu-item">FAQs </a></li>
+                                            {/* <li><a href="Whitepaper" class="sub-menu-item">Whitepaper </a></li> */}
+                                            <li><a href="Contact" class="sub-menu-item">Contact Us</a></li>
+                                        </ul>
                                 </li>
-                            </ul>
+                            <li>
+                                <a href="https://app.3dprintable.io" target="_blank" rel="noopener noreferrer" id="a" >
+                                    <button class="btn btn-primary">Enter App</button>
+                                </a>
+                            </li>
+                        </ul>
                     </div>
                     {/* End navigation */}
 				</div>
-		</header>
-    {/* End NavBar */}
+		    </header>
+            {/* End NavBar */}
 
-    <BrowserRouter>
-      <Routes>    
-        <Route path="/" element={<Home/>} onClick={() => closeMenu()}/>
-        <Route path="/about" element={<About/>} onClick={() => closeMenu()}/>
-        <Route path="/contact" element={<Contact/>} onClick={() => closeMenu()}/>
-        <Route path="/careers" element={<Careers/>} onClick={() => closeMenu()}/>
-        <Route path="/features" element={<Features/>} onClick={() => closeMenu()}/>
-        <Route path="/faqs" element={<FAQs/>} onClick={() => closeMenu()}/>
-        <Route path="/team" element={<Team/>} onClick={() => closeMenu()}/>
-        <Route path="/roadmap" element={<Roadmap/>} onClick={() => closeMenu()}/>
-        <Route path="/whitepaper" element={<Whitepaper/>} onClick={() => closeMenu()}/>
-        <Route path="/error" element={<Error/>} onClick={() => closeMenu()}/>
-      </Routes>
-    </BrowserRouter>
+            <BrowserRouter>
+            <Routes>    
+                <Route path="/" element={<Home/>} onClick={() => closeMenu()}/>
+                <Route path="/about" element={<About/>} onClick={() => closeMenu()}/>
+                <Route path="/contact" element={<Contact/>} onClick={() => closeMenu()}/>
+                <Route path="/careers" element={<Careers/>} onClick={() => closeMenu()}/>
+                <Route path="/features" element={<Features/>} onClick={() => closeMenu()}/>
+                <Route path="/faqs" element={<FAQs/>} onClick={() => closeMenu()}/>
+                <Route path="/team" element={<Team/>} onClick={() => closeMenu()}/>
+                <Route path="/roadmap" element={<Roadmap/>} onClick={() => closeMenu()}/>
+                <Route path="/whitepaper" element={<Whitepaper/>} onClick={() => closeMenu()}/>
+                <Route path="/error" element={<Error/>} onClick={() => closeMenu()}/>
+            </Routes>
+            </BrowserRouter>
 
-    {/* Start Footer */}
-    <footer class="bg-footer">
-        <div class="container-fluid px-0">
-            <div class="py-4">
-                <div class="container">
-                    <div class="row align-items-center">
-                        <div class="col-md-8">
-                            <div class="section-title">
-                                <div class="d-flex">
-                                    <i class="uil uil-envelope-star display-4 text-white title-dark"></i>
-                                    <div class="flex-1 ms-md-4 ms-3">
-                                        <h4 class="fw-medium text-light title-dark mb-1">Get the latest 3DPrintable news in your inbox.</h4>
-                                        <p class="text-white-50 mb-0">Sign up and receive update to date news via email.</p>
+            {/* Start Footer */}
+            <footer class="bg-footer">
+                <div class="container-fluid px-0">
+                    <div class="py-4">
+                        <div class="container">
+                            <div class="row align-items-center">
+                                <div class="col-md-8">
+                                    <div class="section-title">
+                                        <div class="d-flex">
+                                            <i class="uil uil-envelope-star display-4 text-white title-dark"></i>
+                                            <div class="flex-1 ms-md-4 ms-3">
+                                                <h4 class="fw-medium text-light title-dark mb-1">Get the latest 3DPrintable news in your inbox.</h4>
+                                                <p class="text-white-50 mb-0">Sign up and receive update to date news via email.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-4 mt-4 mt-sm-0">
+                                    <div class="text-md-end ms-5 ms-sm-0">
+                                        <a href="/#" id="a" class="btn btn-primary">Subscribe Now</a>
                                     </div>
                                 </div>
                             </div>
                         </div>
+                    </div>
+                </div>
 
-                        <div class="col-md-4 mt-4 mt-sm-0">
-                            <div class="text-md-end ms-5 ms-sm-0">
-                                <a href="/#" id="a" class="btn btn-primary">Subscribe Now</a>
+                <div class="footer-py-30 footer-border-top">
+                    <div class="container">
+                        <div class="row align-items-center">
+                            <div class="col-sm-8">
+                                <div class="text-sm-start text-center">
+                                    <p class="mb-0 text-foot">© 2022 3DPrintable </p>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </footer>
+            {/* End Footer */}
         </div>
-
-        <div class="footer-py-30 footer-border-top">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-sm-8">
-                        <div class="text-sm-start text-center">
-                            <p class="mb-0 text-foot">© 2022 3DPrintable </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
-    {/* End Footer */}
-    </div>
-  );
+    );
 }
 
 export default App;
